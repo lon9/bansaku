5f8dc88ad14c78a00338b5b515ab757c86092085
Rompei <rompei0924@gmail.com>
2015-10-18 21:32:06 +0900

    Add function for limit rate.

--- controllers/api_controller.go
+++ controllers/api_controller.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Rompei/zepher-bansaku/models"
 	"github.com/garyburd/redigo/redis"
 	"github.com/labstack/echo"
+	"net"
 	"net/http"
 )
 
@@ -19,4 +20,29 @@ func APIBansakuGetHandler(c *echo.Context) error {
 		Count: count,
 	}
 	return c.JSON(http.StatusOK, &bansaku)
+}
+
+func checkRateLimit(con redis.Conn, c *echo.Context) bool {
+	ip, _, err := net.SplitHostPort(c.Request().RemoteAddr)
+	if err != nil {
+		panic(err)
+	}
+	current, err := redis.Int(con.Do("LLEN", ip))
+	if err == nil && current > 10 {
+		return false
+	}
+	_, err = redis.Bool(con.Do("EXISTS", "ip"))
+	if err != nil {
+		con.Send("MULTI")
+		con.Send("RPUSH", "ip", ip)
+		con.Send("EXPIRE", "ip", 1)
+		_, err := con.Do("EXEC")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		con.Do("RPUSHX", "ip", ip)
+	}
+	return true
+
 }
